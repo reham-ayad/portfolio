ae21a3418f7163bf5bba7904a95f418a7fea44c2
reham <ryhamyad9@gmail.com.com>
2026-07-16 22:33:17 +0300

    Add about and skills sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './features/home/hero/hero';
 import { Navbar } from './core/components/navbar/navbar';
+import { About } from './features/home/about/about';
+import { Skills } from './features/home/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero,Navbar],
+  imports: [RouterOutlet, Hero,Navbar,About,Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
